01071d3b2cba3dee7aa4b699b3ae2070e537bc48
Isbha19 <119472126+Isbha19@users.noreply.github.com>
2024-05-27 12:33:44 +0400

    keeping one mssge

--- git demo/Program.cs
+++ git demo/Program.cs
@@ -1,11 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World changes again changes!");
-Console.WriteLine("Hello, World changes again changes!");
-Console.WriteLine("Hello, World changes again changes!");
-Console.WriteLine("Hello, World changes again changes!");
-Console.WriteLine("Hello, World changes again changes!");
-Console.WriteLine("Hello, World changes again changes!");
-Console.WriteLine("Hello, World changes again changes!");
+
 
 
 
